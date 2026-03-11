fb984dfc7cf9d7d5a6653499f413306880126449
Jagan Javid (codejavid) <codejavid1@gmail.com>
2026-03-11 20:12:44 +0530

    Delete react/review-app/src directory

--- react/review-app/src/component/sharder/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Button = ({children, version, type, isDisabled}) => {
-  return (
-    <button className={`btn btn-${version}`} type={type} disabled={isDisabled}>{children}</button>
-  )
-}
-
-export default Button
